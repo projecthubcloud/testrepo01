d0ba80ab8ad05560d1a1bfd2cfef0b8e504fac41
projecthubcloud <projecthubcloud>
2024-12-21 00:27:20 +0000

    Pushed from Locofy Builder

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         "color-9": "#ecebe0",
         darkslateblue: "#2a2c77",
         gold: "#ffe012",
+        "color-10": "#fff",
+        black: "#000",
       },
       spacing: {},
       fontFamily: {
@@ -25,14 +27,29 @@ module.exports = {
       "7xl": "26px",
       lgi: "19px",
       "5xl": "24px",
+      "17xl": "36px",
+      "3xl": "22px",
+      "10xl": "29px",
+      "55xl-5": "74.5px",
+      "26xl": "45px",
+      "41xl": "60px",
       inherit: "inherit",
     },
     screens: {
       mq1725: {
         raw: "screen and (max-width: 1725px)",
       },
+      mq1525: {
+        raw: "screen and (max-width: 1525px)",
+      },
+      mq1225: {
+        raw: "screen and (max-width: 1225px)",
+      },
       mq900: {
         raw: "screen and (max-width: 900px)",
+      },
+      mq850: {
+        raw: "screen and (max-width: 850px)",
       },
       mq450: {
         raw: "screen and (max-width: 450px)",
